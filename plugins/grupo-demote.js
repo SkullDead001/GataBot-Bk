b86da41ce0a131bd32cad0f9958d7ad25a655930
SkullDead001 <86022234+SkullDead001@users.noreply.github.com>
2025-10-28 20:28:49 -0600

    Actualizar grupo-demote.js

--- plugins/grupo-demote.js
+++ plugins/grupo-demote.js
@@ -1,48 +1,66 @@
-let handler = async (m, {conn, usedPrefix, text, command}) => {
-if (isNaN(text) && !text.match(/@/g)) {
-} else if (isNaN(text)) {
-var number = text.split`@`[1]
-} else if (!isNaN(text)) {
-var number = text
-}
+let handler = async (m, { conn, usedPrefix, text, command }) => {
+    let number;
 
-if (!text && !m.quoted)
-return conn.sendButton(
-m.chat,
-wm,
-lenguajeGB['smsMalused3']() + `*${usedPrefix + command} @${global.owner[0][0]}*`,
-null,
-[[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]],
-fkontak,
-m
-)
-if (number.length > 13 || (number.length < 11 && number.length > 0))
-return conn.sendButton(
-m.chat,
-wm,
-lenguajeGB['smsDemott']() + `*${usedPrefix + command} @${global.owner[0][0]}*`,
-null,
-[[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]],
-fkontak,
-m
-)
+    if (isNaN(text) && !text.match(/@/g)) {
+        // No hacemos nada, número inválido
+    } else if (isNaN(text)) {
+        number = text.split`@`[1];
+    } else if (!isNaN(text)) {
+        number = text;
+    }
 
-try {
-if (text) {
-var user = number + '@s.whatsapp.net'
-} else if (m.quoted.sender) {
-var user = m.quoted.sender
-} else if (m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-}
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, lenguajeGB['smsAvisoEG']() + lenguajeGB['smsDemott3'](), fkontak, m)
-}
-}
-handler.command = /^(demote|quitarpoder|quitaradmin)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-export default handler
+    // Validaciones
+    if (!text && !m.quoted) {
+        return await conn.sendButton(
+            m.chat,
+            wm,
+            lenguajeGB['smsMalused3']() + `\n*${usedPrefix + command} @${global.owner[0][0]}*`,
+            null,
+            [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]],
+            fkontak,
+            m
+        );
+    }
+
+    if (number?.length > 13 || (number?.length < 11 && number?.length > 0)) {
+        return await conn.sendButton(
+            m.chat,
+            wm,
+            lenguajeGB['smsDemott']() + `\n*${usedPrefix + command} @${global.owner[0][0]}*`,
+            null,
+            [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]],
+            fkontak,
+            m
+        );
+    }
+
+    try {
+        var user;
+        if (text) {
+            user = number + '@s.whatsapp.net';
+        } else if (m.quoted?.sender) {
+            user = m.quoted.sender;
+        } else if (m.mentionedJid?.[0]) {
+            user = number + '@s.whatsapp.net';
+        }
+    } catch (e) {
+        console.error(e);
+        return;
+    } finally {
+        if (user) {
+            await conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+            await conn.sendMessage(
+                m.chat,
+                { text: lenguajeGB['smsAvisoEG']() + lenguajeGB['smsDemott3']() },
+                { quoted: m }
+            );
+        }
+    }
+};
+
+handler.command = /^(demote|quitarpoder|quitaradmin)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
