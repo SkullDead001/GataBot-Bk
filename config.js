bf0a1ed4ee024123af9a43f34132fdf1bbb9238d
SkullDead001 <86022234+SkullDead001@users.noreply.github.com>
2025-10-28 20:43:00 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -13,10 +13,12 @@ import { ar, en, es, id, pt } from './lib/idiomas/total-idiomas.js'
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-'5214531287294', 
-'593968263524',
-'5492916450307',
-'5215539356057'
+'14318142241', 
+'5217152613752',
+'5215539356057',
+'5215585734703',
+'5214791791766',
+'5214813800392,
 ]
 
 global.mods = []
